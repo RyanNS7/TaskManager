92e17fa8efaaec65b6369514fe84cc37f7ad4acc
Ryanns7 <ryanns777@gmail.com>
2026-04-24 12:05:34 -0300

    feat: creation Task entitie

---
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/entities/Task.java
@@ -0,0 +1,45 @@
+package br.com.project.TaskManager.src.entities;
+
+import br.com.project.TaskManager.src.Status;
+
+import java.util.UUID;
+
+public class Task {
+
+    private UUID task_id;
+    private String title;
+    private String description;
+    private Status status;
+
+    public UUID getTask_id() {
+        return task_id;
+    }
+
+    public void setTask_id(UUID task_id) {
+        this.task_id = task_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
