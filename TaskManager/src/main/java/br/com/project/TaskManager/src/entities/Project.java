b57bc680a2bad16267512d0c1a47d2ab2ac5ae1f
Ryanns7 <ryanns777@gmail.com>
2026-05-02 17:00:51 -0300

    refactor: The tasks of the Project class entity were changed from 'LIST' to 'SET'

--- TaskManager/src/main/java/br/com/project/TaskManager/src/entities/Project.java
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/entities/Project.java
@@ -1,11 +1,13 @@
 package br.com.project.TaskManager.src.entities;
 
+import java.util.Set;
 import java.util.UUID;
 
 public class Project {
 
     private UUID id;
     private String name_project;
+    private Set<Task> tasks;
 
     public UUID getId() {
         return id;
@@ -15,11 +17,19 @@ public class Project {
         this.id = id;
     }
 
-    public String getName_project() {
+    public String getNameProject() {
         return name_project;
     }
 
-    public void setName_project(String name_project) {
+    public void setNameProject(String name_project) {
         this.name_project = name_project;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
     }
 }
