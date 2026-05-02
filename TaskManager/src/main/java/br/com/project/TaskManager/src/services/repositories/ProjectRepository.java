11d17970041f86d3da0a54a088e66ae679cdef26
Ryanns7 <ryanns777@gmail.com>
2026-05-02 16:55:33 -0300

    repository of project use cases

--- TaskManager/src/main/java/br/com/project/TaskManager/src/services/repositories/ProjectRepository.java
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/services/repositories/ProjectRepository.java
@@ -2,12 +2,14 @@ package br.com.project.TaskManager.src.services.repositories;
 
 import br.com.project.TaskManager.src.DTO.ProjectDTO;
 import br.com.project.TaskManager.src.entities.Project;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProjectRepository {
 
-    Project createProject(ProjectDTO project);
+    Project createProject(Project project);
     Boolean deleteProject(UUID id_project);
     Project findProjectByID(UUID id_project);
 
--- TaskManager/src/main/java/br/com/project/TaskManager/src/services/repositories/TaskRepository.java
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/services/repositories/TaskRepository.java
@@ -2,9 +2,11 @@ package br.com.project.TaskManager.src.services.repositories;
 
 import br.com.project.TaskManager.src.DTO.TaskDTO;
 import br.com.project.TaskManager.src.entities.Task;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository {
 
     Task createTask(TaskDTO task, UUID project_id);
