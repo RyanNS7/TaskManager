6d8e6897d92144a996c1147da0b181bcffc79c6f
Ryanns7 <ryanns777@gmail.com>
2026-05-05 13:34:52 -0300

    refactor: Adding the project ID to the task's DTO

--- TaskManager/src/main/java/br/com/project/TaskManager/src/DTO/TaskDTO.java
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/DTO/TaskDTO.java
@@ -2,5 +2,4 @@ package br.com.project.TaskManager.src.DTO;
 
 import java.util.UUID;
 
-public record TaskDTO(String title, String description, UUID projectID) {
-}
+public record TaskDTO(String title, String description, UUID projectID) {}
