94536f285444838ef1a1a27cb18a7b371183012b
Ryanns7 <ryanns777@gmail.com>
2026-05-07 16:39:25 -0300

    feat: Error handling in case the enum value is invalid.

--- TaskManager/src/main/java/br/com/project/TaskManager/src/adapters/in/controllers/advice/RestExceptionHandler.java
+++ TaskManager/src/main/java/br/com/project/TaskManager/src/adapters/in/controllers/advice/RestExceptionHandler.java
@@ -4,11 +4,17 @@ import br.com.project.TaskManager.src.exceptions.InternalServerError;
 import br.com.project.TaskManager.src.exceptions.InsufficientCharactersException;
 import br.com.project.TaskManager.src.exceptions.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 import java.time.LocalDateTime;
 
@@ -55,4 +61,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @Override
+    protected @Nullable ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "Invalid value for enum",
+                request.getDescription(false)
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
